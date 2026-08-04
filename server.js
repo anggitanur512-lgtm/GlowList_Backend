a73780d6e066c457e1e0f0ca395c7cc0a29282be
anggitanur512-lgtm <anggitanur512@gmail.com>
2026-08-04 13:39:50 +0700

    comiittttt

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
 const express = require('express');
 const app = express();
+const mysql = require('mysql2')
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'glowlist_db'
+});
+db.connect(err => {
+    if (err) {
+        console.error('Gagal konek ke database:', err)
+    } else {
+        console.log('berhasil konek ke database GlowList')
+    }
+})
 const PORT = 3001;
 
 app.use(express.json());
@@ -7,7 +21,13 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send('Selamat datang di GlowList Awas Apiiiii!');
 })
-
+app.get('/produk', (req, res) => {
+    const sql = 'SELECT * FROM produk';
+    db.query(sql, (err, results) => {
+        if (err) return res.status(500).json({ error: err});
+        res.json(results);
+    })
+})
 app.listen(PORT, () => {
     console.log(`Server GlowList jalan di http://localhost:${PORT}`);
 })
